55fc7b8e31a208e67c2fd55e52fbdb53f2bfb316
Gracious <graciousnguwo56@gmail.com>
2021-04-14 11:38:17 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
 
